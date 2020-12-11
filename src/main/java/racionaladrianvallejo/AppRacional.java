d12cee7ffec3634a73932948d1df3b45690fc2c1
adrivr99 <adri.vallejo.99@gmail.com>
2020-12-11 22:19:51 +0100

    Creación dos racionales

---
+++ src/main/java/racionaladrianvallejo/AppRacional.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package racionaladrianvallejo;
+
+/**
+ *
+ * @author adriv
+ */
+public class AppRacional {
+    public static void main(String[] args) {
+        Racional z = new Racional(4, 5);
+        Racional k = new Racional(3, 4);
+    }
+}
